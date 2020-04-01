5c87a7d4dcdceac976f3e774f56d24ab9cda492a
justinmassiot <justinmassiot@users.noreply.github.com>
2020-04-01 16:40:19 +0200

    Trying to restrict DownGit usage to the current GitHub organization

--- app/home/home.js
+++ app/home/home.js
@@ -30,9 +30,9 @@ homeModule.config([
                     $scope.downloadedFiles = {val: 0};
                     $scope.totalFiles = {val: 0};
 
-                    var templateUrl = "https?://github.com/.+/.+";
+                    var templateUrl = "https?://github.com/Altium-Designer-addons/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://minhaskamal.github.io/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://altium-designer-addons.github.io/DownGit/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
